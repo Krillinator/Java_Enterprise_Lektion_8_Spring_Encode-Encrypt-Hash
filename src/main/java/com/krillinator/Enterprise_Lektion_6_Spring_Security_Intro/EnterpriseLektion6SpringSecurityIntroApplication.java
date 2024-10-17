72c81fcb2e63c8bbd1895c286a28bd5ec1a458f6
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2024-10-17 13:04:48 +0200

    Initial Commit

---
+++ src/main/java/com/krillinator/Enterprise_Lektion_6_Spring_Security_Intro/EnterpriseLektion6SpringSecurityIntroApplication.java
@@ -0,0 +1,13 @@
+package com.krillinator.Enterprise_Lektion_6_Spring_Security_Intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseLektion6SpringSecurityIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseLektion6SpringSecurityIntroApplication.class, args);
+	}
+
+}
